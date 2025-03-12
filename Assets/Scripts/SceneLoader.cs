d6dc64856c6e3becaa62f80cad058585750f2af6
TKesling731 <tkesling07@gmail.com>
2025-03-12 18:08:15 -0500

    New Dialogue added to test customer

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,16 +4,28 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public int sceneNumber;
+    private int savedSceneNumber;
+    public int skipScene;
+    public bool skippingScene;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        savedSceneNumber = sceneNumber;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (skippingScene == false)
+        {
+            sceneNumber = savedSceneNumber;
+        }
+    }
+
+    public void SkipIntro()
+    {
+        skippingScene = true;
+        sceneNumber = skipScene;
     }
 
     public void LoadScene()
