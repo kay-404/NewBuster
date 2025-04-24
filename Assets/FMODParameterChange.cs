0facbb886359a801c7a704b37e1b6e98bead6451
brownseanh <117126750+brownseanh@users.noreply.github.com>
2025-04-24 17:07:12 -0500

    Changed music to loop calmer section, transition to intense section on missing person reveal

---
+++ Assets/FMODParameterChange.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using FMODUnity;
+
+public class FMODParameterChange : MonoBehaviour
+{
+    //public FMODUnity.EventReference eventReference;
+    //FMOD.Studio.EventInstance eventInstance;
+    string intensity = "Intensity";
+    FMODUnity.StudioEventEmitter emitter;
+    public int parameterValue;
+
+    GameObject gameObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject = GameObject.Find("Music");
+        emitter = gameObject.GetComponent<FMODUnity.StudioEventEmitter>();
+
+        //eventReference.Path = "event:/MUS_MainMenu";
+        //eventInstance = FMODUnity.RuntimeManager.CreateInstance(eventReference);
+        //eventInstance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ChangeParameterValue();
+
+        if (gameObject.activeInHierarchy == true)
+        {
+            //eventInstance.setParameterByName(intensity, 1);
+            emitter.SetParameter(intensity, parameterValue);
+        }
+    }
+
+    /*
+    void ChangeParameterValue()
+    {
+        OnDisable();
+    }
+
+    void OnDisable()
+    {
+        eventInstance.setParameterByName(intensity, 1);
+    }
+    */
+}
