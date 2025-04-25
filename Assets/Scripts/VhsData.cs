ad144b89231017bf049756ab7ac13731f7ff42e7
TKesling731 <tkesling07@gmail.com>
2025-04-25 02:24:37 -0500

    Receipt Prototype

--- Assets/Scripts/VhsData.cs
+++ Assets/Scripts/VhsData.cs
@@ -1,16 +1,19 @@
 using System.Drawing;
 using UnityEngine;
 using Yarn.Unity;
+using TMPro;
 
 public class VhsData : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public int ratingValue = 0;
+    public int correctRating;
     public bool isReviewed = false;
     public string descriptionDialogue;
     private string ratingDialogue;
 
+    public TMP_Text reviewScoreText;
     public DialogueRunner vhsDescription;
     public DialogueRunner vhsRating;
     void Start()
@@ -35,7 +38,52 @@ public class VhsData : MonoBehaviour
     public void SubmitRating()
     {
         isReviewed = true;
+        GradeRating();
         vhsRating.StartDialogue(ratingDialogue);
+    }
+
+    public void GradeRating()
+    {
+        if (ratingValue - correctRating == 0)
+        {
+            reviewScoreText.text = "Movie Review: A";
+        }
+        else if (ratingValue - correctRating == 1)
+        {
+            reviewScoreText.text = "Movie Review: B";
+        }
+        else if (ratingValue - correctRating == -1)
+        {
+            reviewScoreText.text = "Movie Review: B";
+        }
+        else if (ratingValue - correctRating == 2)
+        {
+            reviewScoreText.text = "Movie Review: C";
+        }
+        else if (ratingValue - correctRating == -2)
+        {
+            reviewScoreText.text = "Movie Review: C";
+        }
+        else if (ratingValue - correctRating == 3)
+        {
+            reviewScoreText.text = "Movie Review: D";
+        }
+        else if (ratingValue - correctRating == -3)
+        {
+            reviewScoreText.text = "Movie Review: D";
+        }
+        else if (ratingValue - correctRating == 4)
+        {
+            reviewScoreText.text = "Movie Review: F";
+        }
+        else if (ratingValue - correctRating == -4)
+        {
+            reviewScoreText.text = "Movie Review: F";
+        }
+        else
+        {
+            reviewScoreText.text = "N/A";
+        }
     }
 
     public void OneStar()
